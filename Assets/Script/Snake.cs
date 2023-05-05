ce1e9bf26ce70b5e8efc3bff2ab51bd1e794ce26
ABEL K ANTONY <abelkantony98@gmail.com>
2023-05-05 18:53:42 +0530

    updated

--- Assets/Script/Snake.cs
+++ Assets/Script/Snake.cs
@@ -6,6 +6,15 @@ public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
 
+    private List<Transform> _segments;
+
+    [SerializeField] Transform segmantPrefab;
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
@@ -28,6 +37,43 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for(int i = _segments.Count-1; i>0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+    }
+
+    private void Grow()
+    {
+        Transform segment =  Instantiate(this.segmantPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for (int i=1; i< _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        this.transform.position = Vector3.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Food")
+        {
+            Grow();
+        }
+        else
+        {
+            ResetState();
+        }
     }
 }
